9c480f0bf9257076a12331f1daf7a284208322de
Kahou <creator.kahou@gmail.com>
2016-09-21 21:44:53 +0800

    Add NumberExtensions

---
+++ NumberExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace System
+{
+    public static class NumberExtensions
+    {
+        /// <summary>
+        /// Returns an indication whether the value is zero.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">The number to convert.</param>
+        /// <returns>true if value is zero; otherwise, false.</returns>
+        static public bool IsZero<T>(this T value) where T : IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+        {
+            return !Convert.ToBoolean(value);
+        }
+    }
+}
+
